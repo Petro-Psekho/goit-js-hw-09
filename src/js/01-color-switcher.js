316fdbcd76e5b474143d8a6cceeaf1a2f132cdb4
Petro Psekho <psekho.pp@gmail.com>
2022-12-13 19:50:50 +0200

    add Event Listener on buttons

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,18 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+console.log(refs.startBtn.attributes);
+console.log(refs.stopBtn.attributes);
+
+refs.startBtn.addEventListener('click', startChangeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
+
+function startChangeColor(e) {
+  console.log('кликнул Start');
+}
+
+function stopChangeColor(e) {
+  console.log('кликнул Stop');
+}
