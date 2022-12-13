36892c4ae9f7071332647af527f2935c9eb166b9
Petro-Psekho <109413413+Petro-Psekho@users.noreply.github.com>
2022-12-13 17:51:27 +0000

    Deploying to gh-pages from @ Petro-Psekho/goit-js-hw-09@316fdbcd76e5b474143d8a6cceeaf1a2f132cdb4 🚀

---
+++ 01-color-switcher.044f71f6.js
@@ -0,0 +1,2 @@
+const t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};console.log(t.startBtn.attributes),console.log(t.stopBtn.attributes),t.startBtn.addEventListener("click",(function(t){console.log("кликнул Start")})),t.stopBtn.addEventListener("click",(function(t){console.log("кликнул Stop")}));
+//# sourceMappingURL=01-color-switcher.044f71f6.js.map
---
+++ 01-color-switcher.aa205ebe.js
@@ -0,0 +1,2 @@
+!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")};console.log(t.startBtn.attributes),console.log(t.stopBtn.attributes),t.startBtn.addEventListener("click",(function(t){console.log("кликнул Start")})),t.stopBtn.addEventListener("click",(function(t){console.log("кликнул Stop")}))}();
+//# sourceMappingURL=01-color-switcher.aa205ebe.js.map
