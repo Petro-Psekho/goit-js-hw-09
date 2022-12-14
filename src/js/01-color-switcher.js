c37634b56f39736554487ab5756863b691488369
Petro Psekho <psekho.pp@gmail.com>
2022-12-14 17:56:24 +0200

    solves task #1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,18 +1,28 @@
 const refs = {
   startBtn: document.querySelector('button[data-start]'),
   stopBtn: document.querySelector('button[data-stop]'),
+  bgColor: document.querySelector('body'),
 };
-
-console.log(refs.startBtn.attributes);
-console.log(refs.stopBtn.attributes);
 
 refs.startBtn.addEventListener('click', startChangeColor);
 refs.stopBtn.addEventListener('click', stopChangeColor);
 
+let intervalId = null;
+
 function startChangeColor(e) {
-  console.log('кликнул Start');
+  refs.startBtn.disabled = true;
+
+  intervalId = setInterval(() => {
+    refs.bgColor.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 }
 
 function stopChangeColor(e) {
-  console.log('кликнул Stop');
+  refs.startBtn.disabled = false;
+
+  clearInterval(intervalId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
