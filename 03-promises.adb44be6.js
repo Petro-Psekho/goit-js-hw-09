89834abd837ca2ab352b95a686441a043f7a5840
Petro-Psekho <109413413+Petro-Psekho@users.noreply.github.com>
2022-12-15 17:39:16 +0000

    Deploying to gh-pages from @ Petro-Psekho/goit-js-hw-09@67a554795a0b4856581c4dd19ad307956f6359f5 🚀

---
+++ 03-promises.adb44be6.js
@@ -0,0 +1,2 @@
+!function(){var o={form:document.querySelector(".form"),submitBtn:document.querySelector(".form__btn")};console.log(o.form),console.log(o.submitBtn)}();
+//# sourceMappingURL=03-promises.adb44be6.js.map
---
+++ 03-promises.b534467e.js
@@ -0,0 +1,2 @@
+const o={form:document.querySelector(".form"),submitBtn:document.querySelector(".form__btn")};console.log(o.form),console.log(o.submitBtn);
+//# sourceMappingURL=03-promises.b534467e.js.map
