67a554795a0b4856581c4dd19ad307956f6359f5
Petro Psekho <psekho.pp@gmail.com>
2022-12-15 19:38:50 +0200

    task #3 in progress

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,5 +1,14 @@
+const refs = {
+  form: document.querySelector('.form'),
+  submitBtn: document.querySelector('.form__btn'),
+};
+
+console.log(refs.form);
+console.log(refs.submitBtn);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
+
   if (shouldResolve) {
     // Fulfill
   } else {
