8510a16b84e0649a958a559cec807b7bc2c25b60
Petro Psekho <psekho.pp@gmail.com>
2022-12-16 16:43:03 +0200

    solves task #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,17 +1,48 @@
+import Notiflix from 'notiflix';
+
 const refs = {
   form: document.querySelector('.form'),
-  submitBtn: document.querySelector('.form__btn'),
+  firstDelayMs: document.querySelector('[name="delay"]'),
+  delayStepMs: document.querySelector('[name="step"]'),
+  amount: document.querySelector('[name="amount"]'),
 };
 
-console.log(refs.form);
-console.log(refs.submitBtn);
+refs.form.addEventListener('submit', submitPromises);
 
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay }); // Fullogfill
+      } else {
+        reject({ position, delay }); // Reject
+      }
+    }, delay);
+  });
+}
 
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+function submitPromises(e) {
+  e.preventDefault();
+
+  let firstDelay = refs.firstDelayMs.valueAsNumber; // Значення инпуту First delay (перша затримка)
+  const delayStep = refs.delayStepMs.valueAsNumber; // Значення инпуту Delay step
+  const amount = refs.amount.valueAsNumber; // Значення инпуту Amount
+
+  for (let i = 1; i <= amount; i++) {
+    const position = i;
+
+    createPromise(position, firstDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    firstDelay += delayStep;
   }
 }
